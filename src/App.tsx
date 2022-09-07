cad717587626bdf7699508f626cebac7a89a2bc6
partyVic <vicwang128@gmail.com>
2022-09-07 22:12:50 +1000

    Add text-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import CodeCell from './components/code-cell'
+import TextEditor from './components/text-editor'
 
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   )
 }
